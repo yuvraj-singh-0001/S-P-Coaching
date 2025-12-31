06929060942d9deb0c738f06c552d8943764e430
yuvraj singh <singhyuvraj8420@gmail.com>
2025-12-31 15:03:00 +0530

    write the randamp api to setup nodejs

---
+++ server/src/controllers/auth/index.js
@@ -0,0 +1,7 @@
+exports.register = async (req, res) => {
+  res.json({ message: "Register API Working" });
+};
+
+exports.login = async (req, res) => {
+  res.json({ message: "Login API Working" });
+};
